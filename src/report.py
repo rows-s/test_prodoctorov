871b06859411f5780d015165fa21aa250abe9c05
vladimirrows <I3rovvser@gmail.com>
2022-04-21 23:47:28 +0300

    report.ReportData.mr_api public->protected

--- src/report.py
+++ src/report.py
@@ -6,18 +6,18 @@ from models import User
 
 class ReportData:
     def __init__(self, mr_api: MedRatingAPI):
-        self.mr_api = mr_api
+        self._mr_api = mr_api
         self.users = self.get_users()
         self.set_todos()
 
     def get_users(self) -> Dict[str, User]:
         """returns dict {`user_id`: :cls:`User`}"""
         return {user_dict['id']: User.from_raw_dict(user_dict)
-                for user_dict in self.mr_api.get_users()}
+                for user_dict in self._mr_api.get_users()}
 
     def set_todos(self):
         """sets todos into users"""
-        for todo in self.mr_api.get_todos():
+        for todo in self._mr_api.get_todos():
             user = self.users.get(todo.get('userId'))
             if user is not None:
                 user.todos.append(todo)
