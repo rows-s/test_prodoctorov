07497f3061d77de3b0999edbf7fe9d97ba47d323
vladimirrows <I3rovvser@gmail.com>
2022-04-22 14:54:47 +0300

    new way to rename camelCase to snake_case

--- src/models.py
+++ src/models.py
@@ -10,25 +10,35 @@ from typing import Dict, Type
 
 @dataclass
 class Model(ABC):
-    """Abstract dataclass Model that provides :meth:`from_raw_dict()`"""
+    """Abstract dataclass Model that provides :meth:`from_json()`"""
+    __camel_to_snake_fields__ = {}
 
     @classmethod
-    def from_raw_dict(cls, _dict):
+    def from_json(cls, _dict):
         """
         Use it if you want to create a :cls:`Model` with submodels from a whole-raw-dict
-        (where submodel also represented as a dict).
+        (where submodel also represented as a `dict`).
 
-        Submodels' dicts will be converted into corresponding :cls:`Model`
+        Submodel's dict will be converted into corresponding :cls:`Model`
         """
         _dict = _dict.copy()
+        _dict = cls._migrate_camel_to_snake(_dict)
         for name, sub_model in cls._get_sub_models().items():
             sub_dict = _dict[name]
-            _dict[name] = sub_model.from_raw_dict(sub_dict)
+            _dict[name] = sub_model.from_json(sub_dict)
         return cls(**_dict)  # noqa
+
+    @classmethod
+    def _migrate_camel_to_snake(cls, _dict):
+        for last_name, new_name in cls.__camel_to_snake_fields__.items():
+            if last_name in _dict:
+                _dict[new_name] = _dict.pop(last_name)
+        return _dict
 
     @classmethod
     @lru_cache(maxsize=None)  # one cache for one class
     def _get_sub_models(cls) -> Dict[str, Type['Model']]:
+        """returns sub models dict with struct {`field_name`: `type`}. result is cached for each class"""
         sub_models = {}
         for _field in fields(cls):
             with suppress(TypeError):  # suppress "clas is not a type" error
@@ -58,12 +68,7 @@ class Company(Model):
     catch_phrase: str
     bs: str
 
-    @classmethod
-    def from_raw_dict(cls, _dict):
-        _dict = _dict.copy()
-        if 'catchPhrase' in _dict:
-            _dict['catch_phrase'] = _dict.pop('catchPhrase')
-        return super().from_raw_dict(_dict)
+    __camel_to_snake_fields__ = {'catchPhrase': 'catch_phrase'}
 
 
 @dataclass
@@ -86,9 +91,4 @@ class Todo(Model):
     title: str
     completed: bool
 
-    @classmethod
-    def from_raw_dict(cls, _dict):
-        _dict = _dict.copy()
-        if 'userId' in _dict:
-            _dict['user_id'] = _dict.pop('userId')
-        return super().from_raw_dict(_dict)
+    __camel_to_snake_fields__ = {'userId': 'user_id'}
