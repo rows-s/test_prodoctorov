76fb43721f5821488dd85d4f3bc9e3688b6a2e00
vladimirrows <I3rovvser@gmail.com>
2022-04-22 16:49:14 +0300

    UserData._set_todos() public -> protected

--- src/report/user_data.py
+++ src/report/user_data.py
@@ -11,14 +11,14 @@ class UserData:
     def __init__(self, mr_api: MedRatingAPI):
         self._mr_api = mr_api
         self.users = self._get_users()
-        self.set_todos()
+        self._set_todos()
 
     def _get_users(self) -> Dict[str, User]:
         """returns dict {`user_id`: :cls:`User`} form MedRatingAPI"""
         return {user_dict['id']: User.from_json(user_dict)
                 for user_dict in self._mr_api.get_users()}
 
-    def set_todos(self):
+    def _set_todos(self):
         """sets todos into corresponding users"""
         for todo_dict in self._mr_api.get_todos():
             user = self.users.get(todo_dict.get('userId'))
