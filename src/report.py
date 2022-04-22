764f477d2b3b4798ef583e430de3618f44a9cc34
vladimirrows <I3rovvser@gmail.com>
2022-04-22 14:55:13 +0300

    Model.from_raw_dict() -> Model.from_json

--- src/report.py
+++ src/report.py
@@ -5,22 +5,23 @@ from models import User, Todo
 
 
 class ReportData:
+    """class provides methods to prepare data for report"""
     def __init__(self, mr_api: MedRatingAPI):
         self._mr_api = mr_api
         self.users = self.get_users()
         self.set_todos()
 
     def get_users(self) -> Dict[str, User]:
-        """returns dict {`user_id`: :cls:`User`}"""
-        return {user_dict['id']: User.from_raw_dict(user_dict)
+        """returns dict {`user_id`: :cls:`User`} form MedRatingAPI"""
+        return {user_dict['id']: User.from_json(user_dict)
                 for user_dict in self._mr_api.get_users()}
 
     def set_todos(self):
-        """sets todos into users"""
+        """sets todos into corresponding users"""
         for todo_dict in self._mr_api.get_todos():
             user = self.users.get(todo_dict.get('userId'))
             if user is not None:
-                user.todos.append(Todo.from_raw_dict(todo_dict))
+                user.todos.append(Todo.from_json(todo_dict))
 
 
 def main():
