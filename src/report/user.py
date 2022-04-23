66eb479061513128bb4644a2f923cd6105bd072c
vladimirrows <I3rovvser@gmail.com>
2022-04-23 13:30:23 +0300

    UserReport styling

--- src/report/user.py
+++ src/report/user.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import List, ClassVar
+from typing import List, ClassVar, Generator, Iterable
 
 from .base import Report
 from ..models import User, Todo
@@ -11,27 +11,27 @@ __all__ = ['UserReport']
 @dataclass
 class UserReport(User, Report):
     """class provides methods for prepare user report"""
-    done_todos: List[Todo] = field(default_factory=list, init=False)
-    undone_todos: List[Todo] = field(default_factory=list, init=False)
     dt_format: ClassVar[str] = '%d.%m.%Y %H:%M'
 
     @property
-    def title(self):
-        return f'Отчёт для {self.company.name}.'
+    def done_todos(self) -> List[Todo]:
+        return [todo for todo in self.todos if todo.completed]
 
     @property
-    def description(self):
-        t_now = datetime.now()
-        desc = f'{self.name} <{self.email}> {t_now.strftime(self.dt_format)}\n'
+    def undone_todos(self) -> List[Todo]:
+        return [todo for todo in self.todos if not todo.completed]
+
+    def get_report_name(self):
+        return self.username
+
+    def get_title(self):
+        return f'Отчёт для {self.company.name}.'
+
+    def get_description(self):
+        dt_now = datetime.now().strftime(self.dt_format)
+        desc = f'{self.name} <{self.email}> {dt_now}\n'
         desc += f'Всего задач: {len(self.todos)}\n'
         return desc
-
-    def add_todo(self, todo: Todo):
-        super().add_todo(todo)
-        if todo.completed:
-            self.done_todos.append(todo)
-        else:
-            self.undone_todos.append(todo)
 
     def _get_done_todos_detail(self):
         return f'Завершённые ' + self._get_todos_detail(self.done_todos) + '\n'
@@ -41,10 +41,21 @@ class UserReport(User, Report):
 
     @staticmethod
     def _get_todos_detail(todos: List[Todo]):
+        """
+        returns string that according to template:
+
+        задачи (`len_of_todos`):\n
+        `todos_titles`
+
+        where titles separated by newline and each is cut down to 48 characters, tail replaced with "..."
+        """
         def cut_title(todo: Todo, limit=48, fill_with='...'):
             return todo.title[:limit] + (fill_with if len(todo.title) > 48 else '')
 
         return f'задачи: ({len(todos)})\n' + '\n'.join(map(cut_title, todos))
 
     def get_report(self):
-        return '\n'.join((self.title, self.description, self._get_done_todos_detail(), self._get_undone_todos_detail()))
+        report_parts = [self.get_title(), self.get_description()]
+        if self.todos:
+            report_parts += [self._get_done_todos_detail(), self._get_undone_todos_detail()]
+        return '\n'.join(report_parts)
