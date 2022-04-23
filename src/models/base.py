710f253793961374b52202b30f513d71dfd46c68
vladimirrows <I3rovvser@gmail.com>
2022-04-23 13:29:08 +0300

    + docsting

--- src/models/base.py
+++ src/models/base.py
@@ -9,11 +9,11 @@ __all__ = ['Model']
 
 @dataclass
 class Model(ABC):
-    """Abstract dataclass Model that provides :meth:`from_json()`"""
+    """Abstract dataclass Model that provides :meth:`from_lazy_json()`"""
     __camel_to_snake_fields__ = {}
 
     @classmethod
-    def from_json(cls, json):
+    def from_lazy_json(cls, json):
         """
         Use it if you want to create a :cls:`Model` with submodels from a whole-raw-dict
         (where submodel also represented as a `dict`).
@@ -22,11 +22,15 @@ class Model(ABC):
         """
         json = cls._migrate_camel_to_snake(json)  # copied inside
         for name, sub_model in cls._get_sub_models().items():
-            json[name] = sub_model.from_json(json[name])
+            json[name] = sub_model.from_lazy_json(json[name])
         return cls(**json)  # noqa
 
     @classmethod
     def _migrate_camel_to_snake(cls, _dict):
+        """
+        changes all `_dict`'s keys that are represented in `cls.__camel_to_snake_fields__ to according values.
+        returns new :cls:`dict`
+        """
         _dict = _dict.copy()
         for last_name, new_name in cls.__camel_to_snake_fields__.items():
             if last_name in _dict:
@@ -39,7 +43,7 @@ class Model(ABC):
         """returns sub models dict with struct {`field_name`: `type`}. result is cached for each class"""
         sub_models = {}
         for _field in fields(cls):
-            with suppress(TypeError):  # suppress "clas is not a type" error
+            with suppress(TypeError):  # suppress "__cls is not a type" error
                 if issubclass(_field.type, Model):
                     sub_models[_field.name] = _field.type
         return sub_models
