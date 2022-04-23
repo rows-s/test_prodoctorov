8ccde850749545048a1be7586ae524da510c47a0
vladimirrows <I3rovvser@gmail.com>
2022-04-23 13:29:28 +0300

    change Protocol to ABC

--- src/report/base.py
+++ src/report/base.py
@@ -1,15 +1,22 @@
-from typing import Protocol
+from abc import ABC, abstractmethod
 
 __all__ = ['Report']
 
 
-class Report(Protocol):
+class Report(ABC):
     """Abstract report class"""
+    @abstractmethod
+    def get_report_name(self):
+        """returns name for the report"""
+
+    @abstractmethod
     def get_title(self) -> str:
         """returns string that represents title"""
 
+    @abstractmethod
     def get_description(self) -> str:
         """returns string that represents description"""
 
+    @abstractmethod
     def get_report(self) -> str:
         """returns string that represents report"""
