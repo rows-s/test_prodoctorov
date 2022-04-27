f1012a05241e6babde91aaee1fbb2c66b365ba42
vladimirrows <I3rovvser@gmail.com>
2022-04-27 17:41:16 +0300

    run.py

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from src.apis import MedRatingAPI
 from src.report import UserReport
-from src.streams import UsersStream
+from src.streams import UserReportsStream
 from src.handlers import FileHandlerWithArchiving
 
 
 def main():
     file_handler = FileHandlerWithArchiving(directory='tasks')
-    for user in UsersStream(MedRatingAPI(), user_cls=UserReport):
-        file_handler.handle_report(user)  # noqa
+    for user in UserReportsStream(MedRatingAPI()):
+        file_handler.handle_report(user)
 
 
 if __name__ == '__main__':
