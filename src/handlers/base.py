e9c5edc3435b9b953c8f5b85e77f4a5a957b1719
vladimirrows <I3rovvser@gmail.com>
2022-04-27 17:19:50 +0300

    docstrings

--- src/handlers/base.py
+++ src/handlers/base.py
@@ -6,7 +6,7 @@ from ..report import Report
 
 
 class Handler(ABC):
-    """Base handler class"""
+    """Base report handler class"""
 
     @abstractmethod
     def handle_report(self, report: Report):
